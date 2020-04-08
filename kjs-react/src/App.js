3d4f47d41070b3b932531e11b548d964cc6c2517
kimjeongseok <dsaako1117@gmail.com>
2020-04-08 17:56:17 +0900

    김정석폴더커밋완료

--- kjs-react/src/App.js
+++ kjs-react/src/App.js
@@ -3,14 +3,8 @@ import Sub from "./Sub";
 
 function App() {
   const value = 28;
-  const name = "하이하";
-  return (
- 
-      <Sub age={value} name={name} />
-
-  );
+  const name = "김긍정";
+  return <Sub age={value} na={name} />;
 }
 
 export default App;
-
- 
--- kjs-react/src/Sub.jsx
+++ kjs-react/src/Sub.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Sub(props) {
   const abc = props.age;
-  const abcd = props.name;
+  const abcd = props.na;
   console.log(abc);
   console.log(abcd);
   return <div>{`${abcd}의 나이는 ${abc}살 입니다.`}</div>;
